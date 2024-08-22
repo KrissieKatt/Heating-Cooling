df58db5da965334511d663fc7e23772faed47eb8
Krissie Katt <124374091+KrissieKatt@users.noreply.github.com>
2024-08-21 21:31:12 -0400

    Update heating.js

---
+++ heatingcooling/heating.js
@@ -0,0 +1,10 @@
+let actualTemp = 70; // Example actual temperature
+let desiredTemp = 75; // Example desired temperature
+
+if (actualTemp > desiredTemp) {
+    console.log("Run A/C");
+} else if (actualTemp < desiredTemp) {
+    console.log("Run heat");
+} else {
+    console.log("Standby");
+}
